57b171ddbd3b2356c9eca34e20c23452650782e4
Pablo <pablonietoperez369@gmail.com>
2024-05-09 00:46:41 -0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,49 @@
+$(function() {
+  $(".1").click(function(e) {
+    e.preventDefault();
+    $(".over").toggleClass("open");
+  });
+});
+
+$(function() {
+  $(".cont-section-1>div:nth-child(2)").click(function(e) {
+    e.preventDefault();
+    $(".over2").toggleClass("open");
+  });
+});
+
+$(function() {
+  $(".cont-section-2>div:nth-child(1)").click(function(e) {
+    e.preventDefault();
+    $(".over3").toggleClass("open");
+  });
+});
+
+$(function() {
+  $(".cont-section-2>div:nth-child(2)").click(function(e) {
+    e.preventDefault();
+    $(".over4").toggleClass("open");
+  });
+});
+
+$(function() {
+  $(".cont-section-2>div:nth-child(3)").click(function(e) {
+    e.preventDefault();
+    $(".over5").toggleClass("open");
+  });
+});
+
+
+$(function() {
+  $(".cont-section-3>div:nth-child(1)").click(function(e) {
+    e.preventDefault();
+    $(".over6").toggleClass("open");
+  });
+});
+
+
+
+
+
+
+
---
+++ MD/heade.md
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="CSS/normalize.css">
+<link rel="stylesheet" href="CSS/styles.css">
+<link rel="stylesheet" href="CSS/fonts.css">
+<link rel="icon" type="image/x-icon" href="IMG/icon.png">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.slim.min.js"></script>
+<script src="JS/script.js"></script>
